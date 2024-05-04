6b41c414a0388ae18d86b5d86b42c73f920444e1
Crantly1 <77400072+ChrisAntley1@users.noreply.github.com>
2024-05-04 15:07:03 -0400

    created menu and menuItems in DB; migrated DB

---
+++ db/migrate/20240504190243_create_menu_items.rb
@@ -0,0 +1,11 @@
+class CreateMenuItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :menu_items do |t|
+      t.string :name
+      t.decimal :price, precision: 8, scale: 2
+      t.references :menu, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
