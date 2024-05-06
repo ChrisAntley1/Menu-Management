5e6c43818e1b09869a558e201aca2b8f46845bd1
Crantly1 <77400072+ChrisAntley1@users.noreply.github.com>
2024-05-05 22:08:51 -0400

    added validations to models; added association with menu_items to menu

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,7 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
   
-  validates :name, :price, :menu_id, presence: true
+  validates :name, presence: true
+  validates :price, presence: true, numericality: true
+  validates :menu_id, presence: true, numericality: { only_integer: true, message: "must be an integer" }
 end
